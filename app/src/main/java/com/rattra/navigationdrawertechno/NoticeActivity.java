4d574912df964d8dd08126bf112bc5a2f39c79e4
ShubhamRattra <shubhamrattra@gmail.com>
2020-09-07 13:53:27 +0530

    added webview

---
+++ app/src/main/java/com/rattra/navigationdrawertechno/NoticeActivity.java
@@ -0,0 +1,51 @@
+package com.rattra.navigationdrawertechno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+
+        webView = findViewById(R.id.web_notice);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setAllowFileAccess(true);
+
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.setWebChromeClient(new WebChromeClient());
+
+
+        webView = findViewById(R.id.web_notice);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.google.com/?gws_rd=ssl");
+
+    }
+
+    //Code For Back Button
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+            Toast.makeText(this,"Opening Notice",Toast.LENGTH_SHORT).show();
+        } else {
+            super.onBackPressed();
+            Toast.makeText(this,"Closing Notice",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/rattra/navigationdrawertechno/ui/home/HomeFragment.java
+++ app/src/main/java/com/rattra/navigationdrawertechno/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.rattra.navigationdrawertechno.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,17 +14,29 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.rattra.navigationdrawertechno.MainActivity;
+import com.rattra.navigationdrawertechno.NoticeActivity;
 import com.rattra.navigationdrawertechno.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    Button btn_Notice;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        btn_Notice = root.findViewById(R.id.button_notice);
+
+        btn_Notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View root) {
+                startActivity(new Intent(getActivity(), NoticeActivity.class));
+            }
+        });
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -31,5 +45,6 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+
     }
 }
